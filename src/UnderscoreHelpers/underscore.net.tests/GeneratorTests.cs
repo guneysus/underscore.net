e69c548173096974499d70241f0f8cc669ba34fe
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-07-13 02:27:55 +0300

    - phone

--- src/UnderscoreHelpers/underscore.net.tests/GeneratorTests.cs
+++ src/UnderscoreHelpers/underscore.net.tests/GeneratorTests.cs
@@ -268,6 +268,42 @@ namespace underscore.net.tests
         {
             Assert.Equal(expected, _.email(s));
         }
+
+        [Theory]
+        [InlineData("545-231-5566", true)]
+        [InlineData("545 231 55 66", true)]
+        [InlineData("545231 55 66", true)]
+        [InlineData("54523155 66", true)]
+        [InlineData("5452315566", true)]
+        [InlineData("545-2315566", true)]
+        [InlineData("545-231-55-66", true)]
+        [InlineData("545 231-55-66", true)]
+        [InlineData("545 231 5566", true)]
+        [InlineData("0545 231 5566", true)]
+        [InlineData("05452315566", true)]
+        [InlineData("0545231 5566", true)]
+        [InlineData("+90 545231 5566", true)]
+        [InlineData("+90 545 231 5566", true)]
+        [InlineData("+90 545 231 55 66", true)]
+        [InlineData("+90-545-231-55-66", true)]
+        [InlineData("+90545-231-55-66", true)]
+        [InlineData("+90545-XXX-XX-XX", false)]
+        [InlineData("+90545-XXX-XXXX", false)]
+        [InlineData("+90545-XXX-ABCD", false)]
+        [InlineData("+90545XXXABCD", false)]
+        [InlineData("(987) 654-3210", true)]
+        [InlineData("(987)654-3210", true)]
+        [InlineData("987-654-3210", true)]
+        [InlineData("9876543210", true)]
+        [InlineData("+1 9876543210", true)]
+        [InlineData("001 9876543210", true)]
+        [InlineData("001 987-654-3210", true)]
+        [InlineData("19876543210", true)]
+        [InlineData("1-987-654-3210", true)]
+        public void Is_phone_number(string number, bool expected)
+        {
+            Assert.Equal(expected, _.phone(number));
+        }
     }
 
     public class StreamHelperTests : UnderscoreTestBase
--- src/UnderscoreHelpers/underscore.net/Underscore.cs
+++ src/UnderscoreHelpers/underscore.net/Underscore.cs
@@ -617,10 +617,23 @@ namespace underscore.net
         public static bool email(string str) => Regex.IsMatch(str, _emailRegex);
 
         /// <summary>
+        /// Determines whether a string is a valid phone number.
+        /// </summary>
+        /// <param name="str">The string to test.</param>
+        /// <returns><c>true</c> for a valid phone number; otherwise, <c>false</c>.</returns>
+        [Pure]
+        public static bool phone(string str) => Regex.IsMatch(str, _phoneNumberRegex);
+
+        /// <summary>
         /// Regular expression for matching an email address.
         /// </summary>
         /// <remarks>General Email Regex (RFC 5322 Official Standard) from emailregex.com.</remarks>
         const string _emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+        /// <summary>
+        /// Regular expression for matching a phone number.
+        /// </summary>
+        const string _phoneNumberRegex = @"^[+]?(\d{1,3})?[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{2}[\s.-]?\d{2}$";
 
         #endregion
     }
